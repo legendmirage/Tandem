a5969b2072900e47f9b86ce379f7949ae70862fc
Yin Fu Chen <yinfu@mit.edu>
2014-05-07 14:02:25 -0400

    updating announcements and messages to use database

--- Tandem/message/views.py
+++ Tandem/message/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+from message.models import Message
 
 # Create your views here.
 def allMessages(request):
-	return render_to_response('templates/message.html')
+	messages = Message.objects.order_by('-timestamp').order_by('read')
+	variables = {'messages': messages}
+	return render_to_response('templates/message.html', RequestContext(request, variables))
