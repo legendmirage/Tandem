b8127b7c8b88b4cd29e33e9bc49c1266a99dcda1
Yin Fu Chen <yinfu@mit.edu>
2014-05-07 00:44:09 -0400

    blahblah

--- Tandem/Tandem/settings.py
+++ Tandem/Tandem/settings.py
@@ -88,10 +88,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
+    #'/Users/jcweaver/Documents/6.813/Tandem/Tandem/static/',
     #'/Users/jacquelinehung/Dropbox/MIT notes/2014 spring/6.831/group project/Tandem/Tandem/static/', #jackie
     '/home/yinfu/Desktop/Tandem/Tandem/Tandem/static/', #yinfu
 )
 
 #TEMPLATE_DIRS = ('C:/Users/Akhil/sylvia/Tandem/Tandem/templates,') #Sylvia's path
 TEMPLATE_DIRS = ('/home/yinfu/Desktop/Tandem/Tandem/Tandem')  #Yinfu's path
+#TEMPLATE_DIRS = ('/Users/jcweaver/Documents/6.813/Tandem/Tandem/') #Joanie's path
 #TEMPLATE_DIRS = ("/Users/jacquelinehung/Dropbox/MIT notes/2014 spring/6.831/group project/Tandem/Tandem/") #jackie's path
