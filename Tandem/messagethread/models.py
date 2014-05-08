f92407341e15f33d7f35c83c53dc7a104c172c80
Yin Fu Chen <yinfu@mit.edu>
2014-05-08 19:01:16 -0400

    adding messagethread

---
+++ Tandem/messagethread/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Messagethread(models.Model):
+	student = models.ForeignKey('student.Student')
+	subject = models.CharField(max_length=100)
+	#unRead = models.BooleanField(default=False)
+	#replied = models.BooleanField(default=False)
