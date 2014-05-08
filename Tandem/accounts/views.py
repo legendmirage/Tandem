a2a3eafdfbc677927695a134b11ad8e86710674f
unknown <Akhil@Akhil-PC.mit.edu>
2014-05-08 02:09:14 -0400

    The system now forces you to register/login in order to see the seating chart. Everything is very ugly and once you login, everyone sees the same thing.

--- Tandem/accounts/views.py
+++ Tandem/accounts/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth import login, authenticate, logout
 from accounts.models import RegistrationForm
+from django.template import RequestContext
+from student.models import Student
 
 # Create your views here.
 # Code taken from Sylvia's thesis project
@@ -27,8 +29,39 @@ def registrationView(request):
     variables['form'] = form
     return render_to_response('registration/register.html', RequestContext(request, variables))
 
+def loginView(request):
+	if request.POST:
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return HttpResponseRedirect('/')
+			else:
+				# Return a 'disabled account' error message
+				return HttpResponseRedirect('/')
+		else:
+			# Return an 'invalid login' error message.
+			return HttpResponseRedirect('/')
+	else:
+		return render_to_response('registration/login.html', RequestContext(request))
+
 def logoutView(request):
     # @summary: 
     #    View to redirect a logout back to the landing page.
     logout(request)
     return HttpResponseRedirect('/')
+
+def homepage(request):
+	if request.user.is_authenticated():
+		students = Student.objects.all()
+		namePairs = students.values_list('firstName', 'lastName')
+		student_names = [str(stud[0]) + ' ' + str(stud[1]) for stud in namePairs]
+		num_students = len(students)
+		student_ids = [stud.id for stud in students]
+
+		variables = {'students' : students, 'student_names' : student_names, 'num_students': num_students, 'student_ids': student_ids}
+
+		return render_to_response('home.html', RequestContext(request, variables))
+	return HttpResponseRedirect('/login/')
--- Tandem/student/views.py
+++ Tandem/student/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from student.models import Student
 from event.models import Event
@@ -14,17 +15,6 @@ def profile(request, student_id):
 	variables = {"student" : student, "events" : events, 'num_events': num_events}
 	#print student.firstName
 	return render_to_response('profile.html', RequestContext(request, variables))
-
-def homepage(request):
-	students = Student.objects.all()
-	namePairs = students.values_list('firstName', 'lastName')
-	student_names = [str(stud[0]) + ' ' + str(stud[1]) for stud in namePairs]
-	num_students = len(students)
-	student_ids = [stud.id for stud in students]
-
-	variables = {'students' : students, 'student_names' : student_names, 'num_students': num_students, 'student_ids': student_ids}
-
-	return render_to_response('home.html', RequestContext(request, variables))
 
 
 # class EditStudentForm(forms.Form):
