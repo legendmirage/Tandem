19163c787c03debe544e7ae00aa6cc6b83204196
Joanie Weaver <jcweaver@jcweaver.local>
2014-05-08 15:46:03 -0400

    trying to get events to add to db

--- Tandem/event/models.py
+++ Tandem/event/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 class Event(models.Model):
 	student = models.ForeignKey('student.Student')
@@ -7,3 +8,9 @@ class Event(models.Model):
 	description = models.TextField()
 	date = models.DateField()
 	attention = models.BooleanField(default=False)
+
+class EventForm(forms.Form):
+	subject=forms.CharField(max_length=100)
+	description=forms.CharField()
+	date=forms.DateField(initial=datetime.date.today)
+	attention=forms.BooleanField(default=False)
--- Tandem/event/views.py
+++ Tandem/event/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+from django import forms
 
 # Create your views here.
 
+def createEvent(request):
+	if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            subject = form.clean_data['subject']
+            description = form.clean_data['description']
+            attention = form.clean_data['attention']
+            student = form.clean_data['student']
+            date = form.clean_data['date']
+            e=Event(student=student,attention=attention,description=description,subject=subject,date=date)
+            e.save()
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'contact.html', {
+        'form': form,
+    })
