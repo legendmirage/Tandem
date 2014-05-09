4de82efa19cf504ec59807dd7a4112a7ee9b89ea
Yin Fu Chen <yinfu@mit.edu>
2014-05-08 20:31:30 -0400

    commiting merge

--- Tandem/message/views.py
+++ Tandem/message/views.py
@@ -6,19 +6,14 @@ from messagethread.models import Messagethread
 
 # Create your views here.
 def allMessages(request):
-<<<<<<< HEAD
 	messages = Message.objects.order_by('-timestamp')
 	threads = Messagethread.objects.all()
 	threadIDs = [t.id for t in threads]
 	variables = {'messages': messages, 'messageThreads': threads, 'threadIDs': threadIDs}
-	return render_to_response('templates/message.html', RequestContext(request, variables))
+	return render_to_response('message.html', RequestContext(request, variables))
 
 def messagesByThread(request, threadID):
 	t = Messagethread.objects.get(id=threadID)
 	messages = Message.objects.filter(thread=t)
 	variables = {'messagesByThread': messages}
-	return render_to_response('templates/message.html', RequestContext(request, variables))
- 
-=======
-	return render_to_response('message.html')
->>>>>>> 6569b85a86d353947c89e1f12e60cbde7fe62def
+	return render_to_response('message.html', RequestContext(request, variables))
